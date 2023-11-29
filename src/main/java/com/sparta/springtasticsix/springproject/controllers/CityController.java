48e511f0e949327b80903816f4cfa975c79e7d30
LukeBWood <boormanluke76@gmail.com>
2023-11-29 16:02:16 +0000

    Added directory and set up controllers

---
+++ src/main/java/com/sparta/springtasticsix/springproject/controllers/CityController.java
@@ -0,0 +1,8 @@
+package com.sparta.springtasticsix.springproject.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+}
---
+++ src/main/java/com/sparta/springtasticsix/springproject/controllers/CountryLanguageController.java
@@ -0,0 +1,7 @@
+package com.sparta.springtasticsix.springproject.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryLanguageController {
+}
