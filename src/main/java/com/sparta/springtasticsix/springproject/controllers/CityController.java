7c4907805ca97fea884e5e3dc15b0fadf186d469
richard <binghamrichard123@gmail.com>
2023-12-01 14:02:42 +0000

    finished get5DistrictsWithLowestPopulation method

--- src/main/java/com/sparta/springtasticsix/springproject/controllers/CityController.java
+++ src/main/java/com/sparta/springtasticsix/springproject/controllers/CityController.java
@@ -2,7 +2,6 @@ package com.sparta.springtasticsix.springproject.controllers;
 
 
 import com.sparta.springtasticsix.springproject.model.entities.CityDTO;
-import com.sparta.springtasticsix.springproject.model.entities.CountryDTO;
 import com.sparta.springtasticsix.springproject.model.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -49,17 +48,15 @@ public class CityController {
     }
 
     @GetMapping("/city/get5LeastPopulatedDistricts")
-    public void get5LeastPopulatedDistricts(){
+    public Map<String, Integer> get5LeastPopulatedDistricts(){
         List<CityDTO> allCities = cityRepository.findAll();
         Map<String, Integer> districtPopulationMap = new HashMap<>();
         districtPopulationMap.put(allCities.get(0).getDistrict(), allCities.get(0).getPopulation());
 
-//      for(int i = 1; i < allCities.size(); i++){
-        for(int i = 1; i < 12; i++){
-            if(!(allCities.get(i).getDistrict().isEmpty() || allCities.get(i).getDistrict() == "-")){
+      for(int i = 1; i < allCities.size(); i++){
+            if(!(allCities.get(i).getDistrict().isEmpty() || Objects.equals(allCities.get(i).getDistrict(), "-"))){
 
                     if (districtPopulationMap.containsKey(allCities.get(i).getDistrict())){
-                        //add population of current city to the districtPopulationMap.value
                         districtPopulationMap.put(allCities.get(i).getDistrict(), districtPopulationMap.get(allCities.get(i).getDistrict()) + allCities.get(i).getPopulation());
                     }
                     else{
@@ -67,8 +64,16 @@ public class CityController {
                     }
             }
         }
-        //at this point we have total population of ALL districts
-        Collections.s
+
+        Map<String, Integer> sortedDistrictPopulationMap = sortMap(districtPopulationMap);
+        Map<String, Integer> districtsWithLowestPopulation = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sortedDistrictPopulationMap.entrySet()) {
+            if (districtsWithLowestPopulation.size() < 5) {
+                districtsWithLowestPopulation.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return sortMap(districtsWithLowestPopulation);
     }
 
     @DeleteMapping("/city/deleteCity/{id}")
@@ -80,6 +85,17 @@ public class CityController {
         } else {
             return "City not found";
         }
+    }
+
+    private Map<String, Integer> sortMap(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        Map<String, Integer> newMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> district : list) {
+            newMap.put(district.getKey(), district.getValue());
+        }
+        return newMap;
     }
 
 }
