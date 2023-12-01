9825bf23308e834680c847e2b3962986c278eac8
LukeBWood <boormanluke76@gmail.com>
2023-12-01 10:40:49 +0000

    Method for finding the country with the most cities works, albeit innefficiently

--- src/main/java/com/sparta/springtasticsix/springproject/model/repositories/CityRepository.java
+++ src/main/java/com/sparta/springtasticsix/springproject/model/repositories/CityRepository.java
@@ -3,6 +3,7 @@ package com.sparta.springtasticsix.springproject.model.repositories;
 import com.sparta.springtasticsix.springproject.model.entities.CityDTO;
 import com.sparta.springtasticsix.springproject.model.entities.CountryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface CityRepository extends JpaRepository<CityDTO, Integer> {
     List<CityDTO> findCityDTOByCountryCode(CountryDTO country);
+
+    List<CityDTO> findByOrderByCountryCode();
+
 }
