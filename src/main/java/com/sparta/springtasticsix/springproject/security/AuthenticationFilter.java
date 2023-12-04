61ff39d8d313cff0f0f21642cb5ba1ddef2ff665
HenriqueMCunha <hcunha@spartaglobal.com>
2023-12-04 08:56:37 +0000

    Uncommented API-KEY section for merging without issues

--- src/main/java/com/sparta/springtasticsix/springproject/security/AuthenticationFilter.java
+++ src/main/java/com/sparta/springtasticsix/springproject/security/AuthenticationFilter.java
@@ -1,38 +1,38 @@
-//package com.sparta.springtasticsix.springproject.security;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.ServletRequest;
-//import jakarta.servlet.ServletResponse;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.filter.GenericFilterBean;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//public class AuthenticationFilter extends GenericFilterBean {
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-//            throws IOException, ServletException {
-//        try {
-//            ApiKeyAuthentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//        } catch (Exception exp) {
-//            HttpServletResponse httpResponse = (HttpServletResponse) response;
-//            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//            PrintWriter writer = httpResponse.getWriter();
-//            writer.print(exp.getMessage());
-//            writer.flush();
-//           writer.close();
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-//
-//}
+package com.sparta.springtasticsix.springproject.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AuthenticationFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        try {
+            ApiKeyAuthentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception exp) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            PrintWriter writer = httpResponse.getWriter();
+            writer.print(exp.getMessage());
+            writer.flush();
+           writer.close();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
--- src/main/java/com/sparta/springtasticsix/springproject/security/SecurityConfig.java
+++ src/main/java/com/sparta/springtasticsix/springproject/security/SecurityConfig.java
@@ -1,26 +1,26 @@
-//package com.sparta.springtasticsix.springproject.security;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .requestMatchers("/country")
-//                .authenticated()
-//                .and()
-//                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
-//
-//}
+package com.sparta.springtasticsix.springproject.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/country")
+                .authenticated()
+                .and()
+                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
